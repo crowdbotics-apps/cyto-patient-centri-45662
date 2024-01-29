fde4678e2d40d68eec4caba983b726aa454a4c4d
Crowdbotics <team@crowdbotics.com>
2024-01-29 17:50:52 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cyto-patient-centri-45662",
+  url: "https://cyto-patient-centri-45662.botics.co",
+  api: "https://cyto-patient-centri-45662.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
